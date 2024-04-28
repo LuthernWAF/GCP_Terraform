8693bfbcc2baea0644003110df8f2e5e2fc7da35
Luthern WAF <balericagcp@gmail.com>
2024-04-28 18:01:56 -0500

    updated

--- main.tf
+++ main.tf
@@ -2,15 +2,61 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.51.0"
+      version = "5.25.0"
     }
   }
 }
 
 provider "google" {
-  project = "<PROJECT_ID>"
+  project = "thisismustafar" 
+  region = "us-central1" 
+  zone = "us-central1-a"
+  credentials = "thisismustafar-a496f9597734.json"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+
+resource "google_storage_bucket" "chewbacca_home_page" {
+  name          = "chewbacca_home_page_dont_copy_this_or_lizzo_4u"
+  location      = "us-central1"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
 }
+
+
+resource "google_compute_network" "auto-vpc-tf" {
+  name = "auto-vpc-tf"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "sub-sg" {
+  name ="sub-sg"
+  network = google_compute_network.auto-vpc-tf.id
+  ip_cidr_range = "10.72.1.0/24"
+  region = "asia-northeast1"
+}
+
+
+#resource "google_compute_network" "custom-vpc-tf" {
+  #name = "custom-vpc-tf"
+ #auto_create_subnetworks = false
+#}
+
+output "auto" {
+  value = google_compute_network.auto-vpc-tf.id
+}
+
+#output "custom" {
+#  value = google_compute_network.custom-vpc-tf.id
+#}
